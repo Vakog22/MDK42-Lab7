01afa17ff5417f90c586f18a8ea92fd388faca5c
Студент <student2@iMac2.local>
2022-10-27 11:37:46 +0300

    second commit

--- app/src/main/java/com/example/valab7/MainActivity.java
+++ app/src/main/java/com/example/valab7/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.valab7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView TVOut = (TextView) findViewById(R.id.tvOut);
+        Button btnOk = (Button) findViewById(R.id.btnOk);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnOk:
+                        // кнопка ОК
+                        TVOut.setText("Нажата кнопка ОК");
+                        break;
+                    case R.id.btnCancel:
+                        // кнопка Cancel
+                        TVOut.setText("Нажата кнопка Cancel");
+                        break;
+                }
+
+            }
+        };
     }
 }
